327b2b874eca0fdbc14d1678879003bb875b4444
Joel Hanson <joelhanson025@gmail.com>
2023-04-09 13:23:37 +0530

    Fix: styling for upload file detail display

--- web/frontend/src/components/UploadArea.tsx
+++ web/frontend/src/components/UploadArea.tsx
@@ -21,12 +21,12 @@ const UploadArea: React.FC<UploadAreaProps> = ({
 
     const highlight = (event: React.DragEvent) => {
         preventDefaults(event);
-        uploadAreaRef.current?.classList.add("bg-sky-300");
+        uploadAreaRef.current?.classList.add("bg-blue-300");
     };
 
     const unhighlight = (event: React.DragEvent) => {
         preventDefaults(event);
-        uploadAreaRef.current?.classList.remove("bg-sky-300");
+        uploadAreaRef.current?.classList.remove("bg-blue-300");
     };
 
     const handleDrop = (event: React.DragEvent) => {
@@ -39,6 +39,13 @@ const UploadArea: React.FC<UploadAreaProps> = ({
         }
     };
 
+    const handleFileInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        const file = event.target.files?.[0];
+        if (file) {
+            handleFileUpload(file);
+            setUploadReady(true);
+        }
+    };
     return (
         <div
             className="relative z-10 mx-auto px-4 pb-4 pt-10 sm:px-6 md:max-w-sm md:px-4 lg:min-h-full lg:flex-auto flex items-center"
@@ -67,14 +74,12 @@ const UploadArea: React.FC<UploadAreaProps> = ({
                         id="dropzone-file"
                         type="file"
                         className="hidden upload-label-information"
+                        onChange={handleFileInputChange}
                     />
-                    <div
-                        id="upload-file-details"
-                        className="mx-auto p-2 my-2 mt-4 text-left text-sm font-mono text-gray-500 bg-gray-100"
-                    >{uploadFileInfo(uploadedFile)}</div>
+                    <>{uploadFileInfo(uploadedFile)}</>
                     <button
                         id="upload-button"
-                        className="w-full inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-sky-600 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        className="w-full inline-flex items-center justify-center px-4 py-2 my-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                         style={{ display: uploadReady ? "block" : "none" }}
                     >
                         Upload
@@ -108,19 +113,28 @@ function uploadFileInfo(file: File | undefined) {
     if (file) {
         console.log("file: ", file);
         return (
-            <>
+            <div className="mx-auto p-2 my-2 mt-4 text-left text-sm font-mono text-gray-500 bg-gray-100">
                 <h2 className="mt-4 text-xl font-medium text-slate-700">
                     {file.name}
                 </h2>
                 <p className="font-mono text-sm order-first leading-7 text-slate-500 float-left inline p-1">
-                    {file.size} bytes
+                    {bytesToSize(file.size)}
                 </p>
                 <p className="text-xs leading-7 text-slate-700 float-right inline p-1">
                     Type: {(file.type || "n/a").toUpperCase()}
                 </p>
-            </>
+            </div>
         );
     }
+}
+
+function bytesToSize(bytes: number) {
+    const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+    if (bytes == 0) return "n/a";
+    if (!bytes) return "n/a";
+    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)).toString());
+    if (i == 0) return bytes + " " + sizes[i];
+    return (bytes / Math.pow(1024, i)).toFixed(1) + " " + sizes[i];
 }
 
 export default UploadArea;
